c20dbe1c077205c652b09bda21efeee65d695dbc
VPKSoft <vpksoft@vpksoft.net>
2025-03-05 07:31:35 +0200

    Rename Index.ts to index.ts.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 // eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
-    entry: ["src/Index.ts"],
+    entry: ["src/index.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     splitting: false,
